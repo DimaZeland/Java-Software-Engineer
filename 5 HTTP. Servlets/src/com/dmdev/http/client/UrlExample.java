190bf64f1963f2ce54ce64485eb755df4a4d73b8
Java Software Engineer <onenewinvest@gmail.com>
2023-01-10 22:13:47 +0300

    feat(5 HTTP.Servlets): add Socket and Http examples

---
+++ 5 HTTP. Servlets/src/com/dmdev/http/client/UrlExample.java
@@ -0,0 +1,28 @@
+package com.dmdev.http.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlExample {
+    public static void main(String[] args) throws IOException {
+//        checkGoogle();
+        URL url = new URL("file:C:\\Users\\Admin\\IdeaProjects\\http-servlets-starter\\src\\com\\dmdev\\http\\socket\\DatagramRunner.java");
+        URLConnection urlConnection = url.openConnection();
+
+        System.out.println(new String(urlConnection.getInputStream().readAllBytes()));
+    }
+
+    private static void checkGoogle() throws IOException {
+        URL url = new URL("https://www.google.com");
+        URLConnection urlConnection = url.openConnection();
+
+//        urlConnection.setDoOutput(true);
+//        try (OutputStream outputStream = urlConnection.getOutputStream()) {
+//        }
+
+        System.out.println();
+    }
+}
