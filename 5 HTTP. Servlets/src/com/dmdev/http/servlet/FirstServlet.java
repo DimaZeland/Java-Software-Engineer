80c494b4c0e1eb7dde9cefae77030e473c5cba01
Java Software Engineer <onenewinvest@gmail.com>
2023-01-12 23:49:17 +0300

    feat(5 HTTP. Servlets): done demo MVC app

--- 5 HTTP. Servlets/src/com/dmdev/http/servlet/FirstServlet.java
+++ 5 HTTP. Servlets/src/com/dmdev/http/servlet/FirstServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -18,18 +17,25 @@ public class FirstServlet extends HttpServlet {
         super.init(config);
     }
 
-//    @Override
-//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.service(req, resp);
-//    }
-
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
-        resp.setContentType("text/html");
-        try (PrintWriter writer = resp.getWriter()) {
-            writer.write("<h1>Hello from First Servlet</h2>");
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try (var writer = resp.getWriter()) {
+            writer.write("<h1>Привет с первого сервлета!</h2>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (var reader = req.getReader();
+             var lines = reader.lines()) {
+            lines.forEach(System.out::println);
         }
     }
 
@@ -38,4 +44,3 @@ public class FirstServlet extends HttpServlet {
         super.destroy();
     }
 }
-
