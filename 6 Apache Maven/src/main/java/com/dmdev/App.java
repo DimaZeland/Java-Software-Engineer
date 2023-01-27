ff51c3a958180377fcd023b2f381820ade2ad77c
Java Software Engineer <onenewinvest@gmail.com>
2023-01-27 22:20:15 +0300

    docs: delete src folders pattern from .gitignore

---
+++ 6 Apache Maven/src/main/java/com/dmdev/App.java
@@ -0,0 +1,16 @@
+package com.dmdev;
+
+import java.util.function.Supplier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Supplier<String> s = ()-> "HelloWorld";
+        System.out.println( s.get());
+    }
+}
---
+++ 7 JUnit 5/src/main/java/com/dmdev/junit/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.dmdev.junit.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserDao {
+    public boolean delete(Integer userId) {
+        try (var connection = DriverManager.getConnection("url", "username", "password")) {
+        return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
