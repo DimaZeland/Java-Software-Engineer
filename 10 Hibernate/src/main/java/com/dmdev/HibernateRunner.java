2e4c84b482e476edd88e86e490d3acd47b0c7052
Java Software Engineer <onenewinvest@gmail.com>
2023-02-05 20:34:28 +0300

    feat(10 Hibernate): add Logger

--- 10 Hibernate/src/main/java/com/dmdev/HibernateRunner.java
+++ 10 Hibernate/src/main/java/com/dmdev/HibernateRunner.java
@@ -4,41 +4,39 @@ import com.dmdev.entity.User;
 import com.dmdev.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class HibernateRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class); // org.slf4j.Logger позволяет добавить любой логгер,
+    // нужно создавать каждый логгер в отдельном классе
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
                 .username("ivan@gmail.com")
                 .lastname("Ivanov")
                 .firstname("Ivan")
                 .build();
+        log.info("User entity is in transient state, object: {}", user); // по правилу хорошего тона значение переменной вставлять не "+", а "через фигурные скобки"
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.trace("User is in persistent state: {}, session {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setFirstname("Sveta");
-//                session2.delete(user);
-//                refresh/merge
-//                User freshUser = session2.get(User.class, user.getUsername());
-//                freshUser.setLastname(user.getLastname());
-//                freshUser.setFirstname(user.getFirstname());
-// PostgreSQL по умолчанию имеет уровень изолированности транзакций READ_COMMITED. Поэтому мы не увидим изменений, по не сделаем commit транзакций
-                Object mergedUser = session2.merge(user); // создание в session mergeUser +
-//                session2.refresh(user); // запрос к БД + наложением изменений с БД на user
-
-                session2.getTransaction().commit(); // наложением изменений в БД с mergeUser
-            }
+            log.warn("User is in detached state: {}, session is closed {}", user, session1);
+        } catch (Exception exception) {
+            log.error("Exception occurred", exception); // при передаче объектов Throwable есть метод без необходимости вставки фигурных скобок в строке
+            throw exception;
         }
     }
 
--- 10 Hibernate/src/main/java/com/dmdev/entity/User.java
+++ 10 Hibernate/src/main/java/com/dmdev/entity/User.java
@@ -8,12 +8,12 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
 // Runtime аннотации, которые позволяют брать актуальные значения во время выполнения программы
 @Data // equals, hashCode, getters & setters methods
 @NoArgsConstructor
