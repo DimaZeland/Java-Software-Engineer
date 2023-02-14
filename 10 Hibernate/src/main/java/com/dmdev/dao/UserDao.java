536f185cab90dcfc219cf38834a39611fe13a814
Java Software Engineer <onenewinvest@gmail.com>
2023-02-14 18:08:25 +0300

    feat(10 Hibernate): add Querydsl examples

--- 10 Hibernate/src/main/java/com/dmdev/dao/UserDao.java
+++ 10 Hibernate/src/main/java/com/dmdev/dao/UserDao.java
@@ -1,12 +1,19 @@
 package com.dmdev.dao;
 
+import com.dmdev.dto.PaymentFilter;
 import com.dmdev.entity.Payment;
 import com.dmdev.entity.User;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 
 import java.util.List;
+
+import static com.dmdev.entity.QCompany.company;
+import static com.dmdev.entity.QPayment.payment;
+import static com.dmdev.entity.QUser.user;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDao {
@@ -17,39 +24,64 @@ public class UserDao {
      * Возвращает всех сотрудников
      */
     public List<User> findAll(Session session) {
-        return session.createQuery("select u from User u", User.class)
-                .list();
+//        return session.createQuery("select u from User u", User.class)
+//                .list();
+
+        return new JPAQuery<User>(session)
+                .select(user)
+                .from(user)
+                .fetch();
     }
 
     /**
      * Возвращает всех сотрудников с указанным именем
      */
     public List<User> findAllByFirstName(Session session, String firstName) {
-        return session.createQuery("select u from User u " +
-                        "where u.personalInfo.firstname = :firstName", User.class)
-                .setParameter("firstName", firstName)
-                .list();
+//        return session.createQuery("select u from User u " +
+//                "where u.personalInfo.firstname = :firstName", User.class)
+//                .setParameter("firstName", firstName)
+//                .list();
+
+        return new JPAQuery<User>(session)
+                .select(user)
+                .from(user)
+                .where(user.personalInfo.firstname.eq(firstName))
+                .fetch();
     }
 
     /**
      * Возвращает первые {limit} сотрудников, упорядоченных по дате рождения (в порядке возрастания)
      */
     public List<User> findLimitedUsersOrderedByBirthday(Session session, int limit) {
-        return session.createQuery("select u from User u order by u.personalInfo.birthDate", User.class)
-                .setMaxResults(limit)
+//        return session.createQuery("select u from User u order by u.personalInfo.birthDate", User.class)
+//                .setMaxResults(limit)
 //                .setFirstResult(offset)
-                .list();
+//                .list();
+
+        return new JPAQuery<User>(session)
+                .select(user)
+                .from(user)
+                .orderBy(user.personalInfo.birthDate.asc())
+                .limit(limit)
+                .fetch();
     }
 
     /**
      * Возвращает всех сотрудников компании с указанным названием
      */
     public List<User> findAllByCompanyName(Session session, String companyName) {
-        return session.createQuery("select u from Company c " +
-                        "join c.users u " +
-                        "where c.name = :companyName", User.class)
-                .setParameter("companyName", companyName)
-                .list();
+//        return session.createQuery("select u from Company c " +
+//                        "join c.users u " +
+//                        "where c.name = :companyName", User.class)
+//                .setParameter("companyName", companyName)
+//                .list();
+
+        return new JPAQuery<User>(session)
+                .select(user)
+                .from(company)
+                .join(company.users, user)
+                .where(company.name.eq(companyName))
+                .fetch();
     }
 
     /**
@@ -57,38 +89,74 @@ public class UserDao {
      * упорядоченные по имени сотрудника, а затем по размеру выплаты
      */
     public List<Payment> findAllPaymentsByCompanyName(Session session, String companyName) {
-        return session.createQuery("select p from Payment p " +
-                        "join p.receiver u " +
-                        "join u.company c " +
-                        "where c.name = :companyName " +
-                        "order by u.personalInfo.firstname, p.amount", Payment.class)
-                .setParameter("companyName", companyName)
-                .list();
+//        return session.createQuery("select p from Payment p " +
+//                        "join p.receiver u " +
+//                        "join u.company c " +
+//                        "where c.name = :companyName " +
+//                        "order by u.personalInfo.firstname, p.amount", Payment.class)
+//                .setParameter("companyName", companyName)
+//                .list();
+
+        return new JPAQuery<Payment>(session)
+                .select(payment)
+                .from(payment)
+                .join(payment.receiver, user)
+                .join(user.company, company)
+                .where(company.name.eq(companyName))
+                .orderBy(user.personalInfo.firstname.asc(), payment.amount.asc())
+                .fetch();
     }
 
     /**
      * Возвращает среднюю зарплату сотрудника с указанными именем и фамилией
      */
-    public Double findAveragePaymentAmountByFirstAndLastNames(Session session, String firstName, String lastName) {
-        return session.createQuery("select avg(p.amount) from Payment p " +
-                        "join p.receiver u " +
-                        "where u.personalInfo.firstname = :firstName " +
-                        "   and u.personalInfo.lastname = :lastName", Double.class)
-                .setParameter("firstName", firstName)
-                .setParameter("lastName", lastName)
-                .uniqueResult();
+    public Double findAveragePaymentAmountByFirstAndLastNames(Session session, PaymentFilter filter) {
+//        return session.createQuery("select avg(p.amount) from Payment p " +
+//                        "join p.receiver u " +
+//                        "where u.personalInfo.firstname = :firstName " +
+//                        "   and u.personalInfo.lastname = :lastName", Double.class)
+//                .setParameter("firstName", firstName)
+//                .setParameter("lastName", lastName)
+//                .uniqueResult();
+        //        List<Predicate> predicates = new ArrayList<>();
+//        if (filter.getFirstName() != null) {
+//            predicates.add(user.personalInfo.firstname.eq(filter.getFirstName()));
+//        }
+//        if (filter.getLastName() != null) {
+//            predicates.add(user.personalInfo.lastname.eq(filter.getLastName()));
+//        }
+        var predicate = QPredicate.builder()
+                .add(filter.getFirstName(), user.personalInfo.firstname::eq)
+                .add(filter.getLastName(), user.personalInfo.lastname::eq)
+                .buildAnd();
+
+        return new JPAQuery<Double>(session)
+                .select(payment.amount.avg())
+                .from(payment)
+                .join(payment.receiver, user)
+                .where(predicate)
+                .fetchOne();
     }
 
     /**
      * Возвращает для каждой компании: название, среднюю зарплату всех её сотрудников. Компании упорядочены по названию.
      */
-    public List<Object[]> findCompanyNamesWithAvgUserPaymentsOrderedByCompanyName(Session session) {
-        return session.createQuery("select c.name, avg(p.amount) from Company c " +
-                        "join c.users u " +
-                        "join u.payments p " +
-                        "group by c.name " +
-                        "order by c.name", Object[].class)
-                .list();
+    public List<Tuple> findCompanyNamesWithAvgUserPaymentsOrderedByCompanyName(Session session) {
+//        return session.createQuery("select c.name, avg(p.amount) from Company c " +
+//                        "join c.users u " +
+//                        "join u.payments p " +
+//                        "group by c.name " +
+//                        "order by c.name", Object[].class)
+//                .list();
+
+        return new JPAQuery<Tuple>(session)
+                .select(company.name, payment.amount.avg())
+                .from(company)
+                .join(company.users, user)
+                .join(user.payments, payment)
+                .groupBy(company.name)
+                .orderBy(company.name.asc())
+                .fetch();
     }
 
     /**
@@ -96,13 +164,26 @@ public class UserDao {
      * больше среднего размера выплат всех сотрудников
      * Упорядочить по имени сотрудника
      */
-    public List<Object[]> isItPossible(Session session) {
-        return session.createQuery("select u, avg(p.amount) from User u " +
-                        "join u.payments p " +
-                        "group by u " +
-                        "having avg(p.amount) > (select avg(p.amount) from Payment p) " +
-                        "order by u.personalInfo.firstname", Object[].class)
-                .list();
+    public List<Tuple> isItPossible(Session session) {
+//        return session.createQuery("select u, avg(p.amount) from User u " +
+//                        "join u.payments p " +
+//                        "group by u " +
+//                        "having avg(p.amount) > (select avg(p.amount) from Payment p) " +
+//                        "order by u.personalInfo.firstname", Object[].class)
+//                .list();
+
+        return new JPAQuery<Tuple>(session)
+                .select(user, payment.amount.avg())
+                .from(user)
+                .join(user.payments, payment)
+                .groupBy(user.id)
+                .having(payment.amount.avg().gt(
+                        new JPAQuery<Double>(session)
+                                .select(payment.amount.avg())
+                                .from(payment)
+                ))
+                .orderBy(user.personalInfo.firstname.asc())
+                .fetch();
     }
 
     public static UserDao getInstance() {
