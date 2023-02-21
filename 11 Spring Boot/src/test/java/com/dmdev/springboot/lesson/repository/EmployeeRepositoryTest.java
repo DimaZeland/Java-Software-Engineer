ad0e31d6b95a03f1eea8533e0a0719e32d9a8fa3
Java Software Engineer <onenewinvest@gmail.com>
2023-02-21 23:02:56 +0300

    feat(11 Spring Boot): add Data JPA

---
+++ 11 Spring Boot/src/test/java/com/dmdev/springboot/lesson/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.dmdev.springboot.lesson.repository;
+
+import com.dmdev.springboot.lesson.IntegrationTestBase;
+import com.dmdev.springboot.lesson.entity.EmployeeEntity;
+import com.dmdev.springboot.lesson.projection.EmployeeNameView;
+import com.dmdev.springboot.lesson.projection.EmployeeNativeView;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EmployeeRepositoryTest extends IntegrationTestBase {
+
+    private static final Integer IVAN_ID = 1;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void testFindById() {
+        Optional<EmployeeEntity> employee = employeeRepository.findById(IVAN_ID);
+        assertTrue(employee.isPresent());
+    }
+
+    @Test
+    void testFindByFirstName() {
+        Optional<EmployeeEntity> employee = employeeRepository.findByFirstNameContaining("va");
+        assertTrue(employee.isPresent());
+    }
+
+    @Test
+    void testFindByFirstNameAndSalary() {
+        List<EmployeeEntity> employees = employeeRepository.findAllByFirstNameAndSalary("Ivan", 1000);
+        assertThat(employees, hasSize(1));
+    }
+
+    @Test
+    void testFindBySalary() {
+        List<EmployeeNameView> employees = employeeRepository.findAllBySalaryGreaterThan(500);
+        assertThat(employees, hasSize(2));
+    }
+
+    @Test
+    void testFindBySalaryNative() {
+        List<EmployeeNativeView> employees = employeeRepository.findAllBySalaryGreaterThanNative(500);
+        assertThat(employees, hasSize(2));
+    }
+
+    @Test
+    void testFindCustomQuery() {
+        List<EmployeeEntity> customQuery = employeeRepository.findCustomQuery();
+        assertThat(customQuery, hasSize(0));
+    }
+}
