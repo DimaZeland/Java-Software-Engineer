57cd32dc7de233bdbd28375d91720546ca5b97b1
Java Software Engineer <onenewinvest@gmail.com>
2023-03-15 06:45:55 +0300

    feat(spring-boot-3): add deploying on Heroku and Docker

---
+++ 11-Spring-Boot/spring-boot-3/16-Deploying/springboot-heroku-demo/src/main/java/com/demo/HomeController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController
+{
+	@Autowired
+	UserRepository userRepo;
+	
+	@RequestMapping("/")
+	public String home(Model model)
+	{
+		model.addAttribute("users", userRepo.findAll());
+		return "home";
+	}
+}
