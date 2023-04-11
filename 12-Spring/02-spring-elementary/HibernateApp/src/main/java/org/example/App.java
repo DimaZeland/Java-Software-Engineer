51ee16df5e973344b52197c184a6c0314dd6b830
Java Software Engineer <onenewinvest@gmail.com>
2023-04-11 11:43:00 +0300

    refactor(spring-elementary): delete comment from App class

--- 12-Spring/02-spring-elementary/HibernateApp/src/main/java/org/example/App.java
+++ 12-Spring/02-spring-elementary/HibernateApp/src/main/java/org/example/App.java
@@ -5,9 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
