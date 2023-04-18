c709976779a804705c2dbc8179cbd56a093d9ae1
Java Software Engineer <onenewinvest@gmail.com>
2023-04-18 06:17:01 +0300

    feat(spring-elementary): add JWT App module

---
+++ 12-Spring/02-spring-elementary/97_JWT_App/src/test/java/com/spring/JWT_App/SpringSecurityAppTests.java
@@ -0,0 +1,13 @@
+package com.spring.JWT_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
