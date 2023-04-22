eac49380219d07b9b6cf5dc68fca71eeb47f063f
Java Software Engineer <onenewinvest@gmail.com>
2023-04-22 23:43:34 +0300

    feat(spring-starter): add DATA JPA Transactions

--- 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -14,6 +14,6 @@ public class ApplicationRunner {
         System.out.println(context.getBeanDefinitionCount());
         System.out.println(context.getBean("pool1"));
         System.out.println(context.getBean(DatabaseProperties.class));
-        System.out.println("16-17");
+        System.out.println("23-30");
     }
 }
