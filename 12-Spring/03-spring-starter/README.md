58baab39b11a26c102e913d9d011edb802a992bd
Java Software Engineer <onenewinvest@gmail.com>
2023-04-22 16:22:26 +0300

    feat(spring-starter): add sql dump files and java entities

--- 12-Spring/03-spring-starter/README.md
+++ 12-Spring/03-spring-starter/README.md
@@ -1,2 +1,1 @@
 # spring-starter
-21-25
--- 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -14,5 +14,6 @@ public class ApplicationRunner {
         System.out.println(context.getBeanDefinitionCount());
         System.out.println(context.getBean("pool1"));
         System.out.println(context.getBean(DatabaseProperties.class));
+        System.out.println("16-17");
     }
 }
