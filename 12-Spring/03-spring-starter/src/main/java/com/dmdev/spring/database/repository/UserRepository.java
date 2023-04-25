2d39217c76ab23f6aea897ced04d2f448c240564
Java Software Engineer <onenewinvest@gmail.com>
2023-04-25 12:00:00 +0300

    feat(spring-starter): add special parameters

--- 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ 12-Spring/03-spring-starter/src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.dmdev.spring.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -17,4 +23,28 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
         nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+        "set u.role = :role " +
+        "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
 }
+
+
+
+
+
+
+
+
+
+
+
+
