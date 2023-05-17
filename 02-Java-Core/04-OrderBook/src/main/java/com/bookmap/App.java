ef599f0041b1a6c86daa29a6be645266a7b8573f
Java Software Engineer <onenewinvest@gmail.com>
2023-05-17 15:30:39 +0300

    refactor(orderBook): delete performe time check

--- 02-Java-Core/04-OrderBook/src/main/java/com/bookmap/App.java
+++ 02-Java-Core/04-OrderBook/src/main/java/com/bookmap/App.java
@@ -3,11 +3,8 @@ package com.bookmap;
 public class App {
 
     public static void main(String[] args) {
-        long time = System.currentTimeMillis();
 
         OrderBook orderBook = new OrderBook();
         orderBook.manage();
-
-        System.out.println(System.currentTimeMillis() - time);
     }
 }
