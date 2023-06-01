599b57819d8e30d34f180c0032cfe2913995eebb
Java Software Engineer <onenewinvest@gmail.com>
2023-06-01 12:54:17 +0300

    feat(04-bets): change data folder type to ConcurrentHashMap<>()

--- 11-Spring-Boot/04-bets REST API/src/main/java/com/spring/bets/services/BetsService.java
+++ 11-Spring-Boot/04-bets REST API/src/main/java/com/spring/bets/services/BetsService.java
@@ -4,61 +4,81 @@ import com.spring.bets.models.Bet;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-public class BetsService
-{
-    private Map<String, Integer> bets = Collections.synchronizedMap(new HashMap<>());
+public class BetsService {
 
-    public BetsService()
-    {
+    public static void main(String[] args) {
+        List<String> cars = List.of("ferrari", "bmw", "audi", "mercedes");
 
+        long start = System.currentTimeMillis();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 1_000_000; j++) {
+                    addBet(cars.get(j % 4), 1);
+                }
+            });
+            threads.add(thread);
+        }
+
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException ignore) {}
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("result: " + bets);
+        System.out.println("duration: " + (end - start) + "ms");
+    }
+
+    private static Map<String, Integer> bets = new ConcurrentHashMap<>();
+
+    public BetsService() {
         bets.put("ferrari", 0);
         bets.put("bmw", 0);
         bets.put("audi", 0);
         bets.put("mercedes", 0);
     }
 
-    public boolean hasCar(String car)
-    {
+    public static boolean hasCar(String car) {
         return bets.containsKey(car);
     }
 
-    public void addBet(String car, Integer dollars)
-    {
-        synchronized (bets)
-        {
+    public synchronized static void addBet(String car, Integer dollars) {
+        bets.merge(car, dollars, Integer::sum);
+//        bets.computeIfAbsent();
+//        bets.computeIfPresent();
+//        bets.putIfAbsent();
+//        bets.getOrDefault();
+//        bets.forEach();
+
+        /*synchronized (bets) {
             Integer totalBet = bets.get(car) + dollars;
-            try
-            {
+            try {
                 Thread.sleep(3000);
 
-            } catch (InterruptedException e)
-            {
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             bets.put(car, totalBet);
-        }
+        }*/
     }
 
-    public List<Bet> getBets(String car)
-    {
-        List<Bet> betsList = new ArrayList<Bet>();
+    public static List<Bet> getBets(String car) {
+        List<Bet> betsList = new ArrayList<>();
 
-        if (car == null)
-        {
-            for (String key : bets.keySet())
-            {
+        if (car == null) {
+            for (String key : bets.keySet()) {
                 Bet bet = Bet.builder().carName(key).betAmount(bets.get(key)).build();
                 betsList.add(bet);
             }
-        } else
-        {
-
+        } else {
             Integer totalBet = bets.get(car);
-            Optional<Integer> optionalInteger = Optional.ofNullable(totalBet);
-
-            Bet bet = Bet.builder().carName(car).betAmount(optionalInteger.orElse(0)).build();
+            Bet bet = Bet.builder().carName(car).betAmount(totalBet).build();
             betsList.add(bet);
         }
 
